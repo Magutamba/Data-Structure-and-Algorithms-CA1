4ede5467ff9eff60f7c0734b71670fc089fff493
Moïse Agutamba Tibasima <magutamba@gmail.com>
2026-03-18 01:31:51 +0000

    Main app and main GUI

---
+++ BiodiversityManagerApp/src/biodiversitymanagerapp/BiodiversityManagerApp.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package biodiversitymanagerapp;
+
+/**
+ *
+ * @author moise
+ */
+public class BiodiversityManagerApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        BiodiversityManagerGUI GUI=new BiodiversityManagerGUI();
+        GUI.setVisible(true);
+    }
+    
+}
